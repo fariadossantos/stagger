f9a06d3a25085127189f3c116e512b794626a062
Faria Quitamba <fariaquitamba5@gmail.com>
2016-06-07 09:59:48 +0100

    fix bug on conection page

--- tools/connect.php
+++ tools/connect.php
@@ -3,7 +3,7 @@
 class connect {
   var $conn;
   function connect (){
-    $this->conn = new PDO('mysql:host=localhost;dbname=stagger', 'root', '');
+    $this->conn = new PDO('mysql:host=localhost;dbname=u163653339_stagr', 'u163653339_root', '1q2w3e4r5t6y');
   }
 }
 
